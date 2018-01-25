f7272088fc985f3379305784dfe5843bec822899
Mohammad Mehdi <mmehdi534@gmail.com>
2018-01-25 19:18:40 +0330

    fragmentes done

--- app/src/main/java/com/maraton/mohammadmehdi/takhfif/MainActivity.java
+++ app/src/main/java/com/maraton/mohammadmehdi/takhfif/MainActivity.java
@@ -3,34 +3,40 @@ package com.maraton.mohammadmehdi.takhfif;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+
     Intent i;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
+
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-             //       i=new Intent(getApplicationContext(),)
-                    //  mTextMessage.setText(R.string.title_home);
+                    transaction.replace(R.id.constrainLayout,new HomeFragment()).commit();
                     return true;
                 case R.id.navigation_add_friend:
-
+                    transaction.replace(R.id.constrainLayout,new Add_FriendFragment()).commit();
                     // mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_transferـmoney:
-
-
+                    transaction.replace(R.id.constrainLayout,new transfer_moneyFragment()).commit();
+                    return true;
                 case R.id.navigation_user:
-
-
+                    transaction.replace(R.id.constrainLayout,new UserFragment()).commit();
+                    return true;
                 case R.id.navigation_chat:
+                    transaction.replace(R.id.constrainLayout,new ChatFragment()).commit();
                     //  mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
@@ -42,5 +48,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.constrainLayout,new HomeFragment()).commit();
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
